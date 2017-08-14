93cb204b4011cd517c7eae94ace324021468142b
virgil9306 <virgil9306@gmail.com>
2017-08-14 19:56:25 +0900

    commit

---
+++ test/index.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import {describe, it, before, after} from 'mocha';
+import {expect} from 'chai';
+import sinon from 'sinon';
+import StatusStepper from '../src/index.js';
+
+describe('<StatusStepper />', () => {
+
+  let wrapper;
+
+});
